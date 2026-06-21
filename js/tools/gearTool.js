4a1364ce9bdd01c36e61a410e17f8adc24f0a91b
dieno-commits-an-arson <ee.pasana.sciencenorth@gmail.com>
2026-06-21 12:19:33 +0800

    Update gearTool.js

--- js/tools/gearTool.js
+++ js/tools/gearTool.js
@@ -3,16 +3,14 @@ import { Gear } from '../models/Gear.js';
 
 export class GearTool {
     pointerDown(e, worldPos) {
-        // Instantiate a new gear exactly at the mouse's world coordinates
         const newGear = new Gear(worldPos.x, worldPos.y);
         state.components.push(newGear);
         
-        // Auto-select the newly created gear
-        state.ui.selectedId = newGear.id;
+        // Push ID to selection array
+        state.ui.selectedIds = [newGear.id];
         window.dispatchEvent(new CustomEvent('selectionChanged'));
     }
 
-    // Required by interface but unused for basic instantiation
     pointerMove(e, worldPos) {}
     pointerUp(e, worldPos) {}
 }
